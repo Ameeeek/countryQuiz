be5ebce060e0d7196d2ab5fb364a98dd61955b43
Ameeeek <ameeeekwork@gmail.com>
2022-05-26 06:47:20 +0800

    add result

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import Question from "./components/question";
 import bg from "./img/background.png";
 import Header from "./components/header";
 function App() {
-   
     return (
         <>
             <img
@@ -11,7 +10,7 @@ function App() {
             />
             <div className="min-h-screen xl:p-12 2xl:p-12 md:p-10 p-6 lg:p-10 flex flex-col item-center justify-center">
                 <Header />
-                <Question  />
+                <Question/> 
             </div>
         </>
     );
--- src/components/question.jsx
+++ src/components/question.jsx
@@ -8,7 +8,7 @@ function Question() {
   const [option, setOption] = useState([]);
   const [request, setRequest] = useState(false);
   const jsConfetti = new JSConfetti();
-  const [is5, setIs5] = useState(false)
+  const [is5, setIs5] = useState(false);
   const quiz = newQuestion("capital-to-country", 3);
   const [count, setCount] = useState(0);
   console.log(count);
@@ -27,59 +27,64 @@ function Question() {
   return (
     <div className=" flex  flex-col text-center item-center  justify-center">
       <div className="answer rounded-xl p-8 popBold z-20 flex bg-white flex-col item-center justify-centers text-center">
-        <div className="xl:z-20 lg:text-lg  sm:text-xs text-xs md:text-2xl  p-2 xl:text-4xl  text-center md:text-center  uppercase">
-          <p className="biru ">{question} is the capital of</p>
-        </div>
-        {option.map((options) => {
-          return (
-            <div className="xl:z-10  sm:z-1 ">
-              <button
-                value={options}
-                onClick={(e) => {
-                  let vlueBaru = e.target.value;
-                  let vlueBaruu = e.target;
-                  console.log(vlueBaruu)
-                  let jawab = document.getElementById("tanya");
-                  console.log(vlueBaru);
-                  if (vlueBaru == answer) {
-                    setRequest(true);
-                    jsConfetti.addConfetti();
-                    vlueBaruu.style.backgroundColor = "green";
-                    // swal({
-                    //   title: "benar",
-                    //   text: "jawaban mu benar!!",
-                    //   icon: "success",
-                    // });
-                    setTimeout(() => {
-                      vlueBaruu.style.backgroundColor = "white";
-
-                    }, 500);
-                  } else {
-                    setRequest(true);
-                    vlueBaruu.style.backgroundColor = "red";
-                    // swal({
-                    //   title: "salah kocak",
-                    //   text: "coba lagi!!",
-                    //   icon: "error",
-                    // });
-                    setCount(count + 1);
-                    if (count == 5) {
-                      swal("selesai!!");
-                      setIs5(true)
-                    }
-                    setTimeout(() => {
-                      vlueBaruu.style.backgroundColor = "white";
-                    }, 500);
-                  }
-                }}
-                id="tanya"
-                className="xl:text-2xl  xl:py-6 py-4 mt-2 md:mt-2 sm:text-sm  text-xs w-full sm:py-4 sm:w-8/12 xl:w-full xl:h-auto tanya rounded-xl hover:bg-orange-400 border-solid  border-2 border-cyan-600"
-              >
-                {options}
-              </button>
+        {is5 ? (
+          <h1>Halo</h1>
+        ) : (
+          <>
+            <div className="xl:z-20 lg:text-lg  sm:text-xs text-xs md:text-2xl  p-2 xl:text-4xl  text-center md:text-center  uppercase">
+              <p className="biru ">{question} is the capital of</p>
             </div>
-          );
-        })}
+            {option.map((options) => {
+              return (
+                <div className="xl:z-10  sm:z-1 ">
+                  <button
+                    value={options}
+                    onClick={(e) => {
+                      let vlueBaru = e.target.value;
+                      let vlueBaruu = e.target;
+                      console.log(vlueBaruu);
+                      let jawab = document.getElementById("tanya");
+                      console.log(vlueBaru);
+                      if (vlueBaru == answer) {
+                        setRequest(true);
+                        jsConfetti.addConfetti();
+                        vlueBaruu.style.backgroundColor = "green";
+                        // swal({
+                        //   title: "benar",
+                        //   text: "jawaban mu benar!!",
+                        //   icon: "success",
+                        // });
+                        setTimeout(() => {
+                          vlueBaruu.style.backgroundColor = "white";
+                        }, 500);
+                      } else {
+                        setRequest(true);
+                        vlueBaruu.style.backgroundColor = "red";
+                        // swal({
+                        //   title: "salah kocak",
+                        //   text: "coba lagi!!",
+                        //   icon: "error",
+                        // });
+                        setCount(count + 1);
+                        if (count == 5) {
+                          swal("selesai!!");
+                          setIs5(true);
+                        }
+                        setTimeout(() => {
+                          vlueBaruu.style.backgroundColor = "white";
+                        }, 500);
+                      }
+                    }}
+                    id="tanya"
+                    className="xl:text-2xl  xl:py-6 py-4 mt-2 md:mt-2 sm:text-sm  text-xs w-full sm:py-4 sm:w-8/12 xl:w-full xl:h-auto tanya rounded-xl hover:bg-orange-400 border-solid  border-2 border-cyan-600"
+                  >
+                    {options}
+                  </button>
+                </div>
+              );
+            })}
+          </>
+        )}
       </div>
     </div>
   );
